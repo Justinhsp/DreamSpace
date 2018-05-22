c16a3cfb36b85387ae54b4e303665e9b1d6fed6a
Justinhsp <1055029008@qq.com>
2018-05-22 11:49:38 +0800

    sign

---
+++ app/src/main/java/com/hycf/example/hsp/ui/activity/WelComeActivity.java
@@ -0,0 +1,24 @@
+package com.hycf.example.hsp.ui.activity;
+
+import com.jaydenxiao.common.base.BaseActivity;
+
+/**
+ * 启动页
+ */
+public class WelComeActivity  extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
